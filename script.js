a7ac823425f3e786625b493b5d79a89a4fd0615e
Brennan Gaunce <brennan.gaunce@kaazing.com>
2014-09-06 18:21:42 -0700

    Hide Next Question button; add alert when questions are over

--- script.js
+++ script.js
@@ -17,20 +17,30 @@ var currentQuestion = 0;
 function setup() {
     var question = document.getElementById("question");
     var answer = document.getElementById("answer");
+    var checkAnswerButton = document.getElementById("checkAnswerButton");
+    var nextQuestionButton = document.getElementById("nextQuestionButton");
 
     showQuestion();
 }
 
 function showQuestion() {
     question.innerHTML = questions[currentQuestion];
+    nextQuestionButton.style.display = "none";
 }
 
 function showAnswer() {
     answer.innerHTML = answers[currentQuestion];
+
+    nextQuestionButton.style.display = "inline";
 }
 
 function showNextQuestion() {
-    nextQuestion = currentQuestion+1;
+    var nextQuestion = currentQuestion+1;
+    if (nextQuestion >= questions.length) {
+        alert("That's all the questions!");
+        return;
+    }
+
     currentQuestion = nextQuestion;
 
     showQuestion();
