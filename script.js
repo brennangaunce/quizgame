e3388ff00d23ceecbdc8a3ddcf8a58138574bb42
Brennan Gaunce <brennan.gaunce@kaazing.com>
2014-09-06 18:56:04 -0700

    Hide Correct/Incorrect buttons at the end of the game

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ var questions = [
     "What's the capital of Kentucky?",
     "What's the capital of California?",
     "What's the capital of Arizona?",
-    "What's the capital of Kansas?"
+    "What's the capital of Kansas?",
+    "Who's the first president of the United States?"
 ];
 
 var answers = [
     "Frankfort",
     "Sacramento",
     "Phoenix",
-    "Topeka"
+    "Topeka",
+    "George Washington"
 ];
 
 var currentQuestion = 0;
@@ -64,6 +66,13 @@ function showNextQuestion() {
     var nextQuestion = currentQuestion+1;
     if (nextQuestion >= questions.length) {
         alert("That's all the questions!\nYou got "+score+" questions right!");
+
+        checkAnswerButton.style.display = "none";
+        correctButton.style.display = "none";
+        incorrectButton.style.display = "none";
+
+        score = 0;
+
         return;
     }
 
