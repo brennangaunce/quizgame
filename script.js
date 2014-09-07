533d665f2d580fd62b4c78e3693cd1c7a2749c73
Brennan Gaunce <brennan.gaunce@kaazing.com>
2014-09-06 18:17:25 -0700

    Added more general next question

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+var questions = [
+    "What's the capital of Kentucky?",
+    "What's the capital of California?",
+    "What's the capital of Arizona?",
+    "What's the capital of Kansas?"
+];
+
+var answers = [
+    "Frankfort",
+    "Sacramento",
+    "Phoenix",
+    "Topeka"
+];
+
+var currentQuestion = 0;
+
 function setup() {
+    var question = document.getElementById("question");
+    var answer = document.getElementById("answer");
+
     showQuestion();
 }
 
 function showQuestion() {
-    var question = document.getElementById("question");
-    question.innerHTML = "What's the capital of Kentucky?";
+    question.innerHTML = questions[currentQuestion];
 }
 
 function showAnswer() {
-    var answer = document.getElementById("answer");
-    answer.innerHTML = "Frankfort";
+    answer.innerHTML = answers[currentQuestion];
+}
+
+function showNextQuestion() {
+    nextQuestion = currentQuestion+1;
+    currentQuestion = nextQuestion;
+
+    showQuestion();
+    answer.innerHTML = "&nbsp;";
 }
