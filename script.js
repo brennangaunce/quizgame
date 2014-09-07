952f9ccc7c7c7f90a0978891e5434355273f8d16
Brennan Gaunce <brennan.gaunce@kaazing.com>
2014-09-06 17:45:47 -0700

    Added script.js with question and answers

---
+++ script.js
@@ -0,0 +1,5 @@
+var question = document.getElementById("question")
+question.innerHTML = "What's the capital of Kentucky?"
+
+var answers = document.getElementById("answers");
+answers.innerHTML = "Frankfort"
